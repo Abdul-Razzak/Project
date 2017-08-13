d75d010292ed86eccaf51013f3c4fb78dec41eab
Abdul-Razzak <abd.razzak.shaikh@gmail.com>
2017-08-13 21:46:13 +0200

    Login and Sign up

---
+++ app/src/main/java/com/learn2crack/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.learn2crack.utils;
+
+public class Constants {
+
+    public static final String BASE_URL = "http://ec2-18-220-154-55.us-east-2.compute.amazonaws.com:3000";
+    public static final String TOKEN = "token";
+    public static final String EMAIL = "email";
+}
