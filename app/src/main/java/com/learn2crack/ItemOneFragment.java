5118bde091c5b9cc76b97493c4197a4c41a70e1b
angryhawk1 <salam_shaikh90@yahoo.com>
2017-08-19 03:10:24 +0200

    Trip buddy logo, added another fragment

--- app/src/main/java/com/learn2crack/ItemOneFragment.java
+++ app/src/main/java/com/learn2crack/ItemOneFragment.java
@@ -125,7 +125,7 @@ public class ItemOneFragment extends Fragment implements
         {
             @Override
             public void onClick(View v){
-                Intent intent1 = new Intent(getActivity(), VenueDetails.class);
+                Intent intent1 = new Intent(getActivity(), FragmentTop.class);
                 startActivity(intent1);//Edited here
             }
         });
--- app/src/main/java/com/learn2crack/utils/Constants.java
+++ app/src/main/java/com/learn2crack/utils/Constants.java
@@ -2,8 +2,8 @@ package com.learn2crack.utils;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://ec2-13-59-103-206.us-east-2.compute.amazonaws.com:3000/";
-    public static final String BASE_API = "http://ec2-13-59-103-206.us-east-2.compute.amazonaws.com:3000/";
+    public static final String BASE_URL = "http://ec2-18-220-239-131.us-east-2.compute.amazonaws.com:3000/";
+    public static final String BASE_API = "http://ec2-18-220-239-131.us-east-2.compute.amazonaws.com:3000/";
     public static final String TOKEN = "token";
     public static final String EMAIL = "email";
 }
